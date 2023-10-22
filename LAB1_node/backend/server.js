cae32edee9e64613c02ebc83e00b3befc4f5c8a8
unknown <ivanzgelagit@gmail.com>
2023-10-22 21:31:42 +0200

    add

--- LAB1_node/backend/server.js
+++ LAB1_node/backend/server.js
@@ -44,6 +44,17 @@ app.use(checkJwt);
 app.use('/', authRoutes);
 app.use('/competition', competitionRoutes);
 
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+      res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+      res.json({ message: '404 Not Found' })
+  } else {
+      res.type('txt').send('404 Not Found')
+  }
+})
+
 const externalUrl = process.env.RENDER_EXTERNAL_URL;
 const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 8080;
 if (externalUrl) {
