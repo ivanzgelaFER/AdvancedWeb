d806fc279f6689381e57f039009a824d29a60b3a
unknown <ivanzgelagit@gmail.com>
2023-10-23 23:53:00 +0200

    Git added create table

--- LAB1_node/backend/db/index.js
+++ LAB1_node/backend/db/index.js
@@ -5,13 +5,35 @@ dotenv.config()
 const pool = new Pool({
     //onnectionString: "postgres://ivan:tYuaocd9TrE8K4M02FxXCkNkzF5ZVMSV@dpg-ckqn2aprfc9c739sft4g-a/web2_lab1_2gge",
     //connectionString: "mysql://ujbtz42hnjir26oo:vAZXe6OGNwhW2MQcUe6h@blamxtxdr4jbeelputqc-mysql.services.clever-cloud.com:3306/blamxtxdr4jbeelputqc"
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port:  process.env.DB_PORT,
-    //ssl: true  //za produkciju ovo moras izmijeniti, treb ti https umjesto http na kojoj je sad
+    connectionString: "postgres://default:c2hpi4tbUwIT@ep-withered-math-75496893.eu-central-1.postgres.vercel-storage.com:5432/verceldb"
+    
+    //user: process.env.DB_USER,
+    //host: process.env.DB_HOST,
+    //database: process.env.DB_DATABASE,
+    //password: process.env.DB_PASS,
+    //port:  process.env.DB_PORT,
+    ,
+    ssl: true  //za produkciju ovo moras izmijeniti, treb ti https umjesto http na kojoj je sad
 });
+
+const sql_create_competition = `CREATE TABLE categories (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name text,
+    vrsta text,
+    win int, 
+    lose int, 
+    draw int
+)`;
+
+const sql_create_game = `CREATE TABLE categories (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    round int,
+    player1 text,
+    player2 text, 
+    competition text, 
+    result int
+)`;
+
 
 module.exports = {
     query: (text, params) => {
