c7839a6b3b45380a81194114ff42960295458393
unknown <ivanzgelagit@gmail.com>
2023-10-23 23:33:14 +0200

    pool

--- LAB1_node/backend/db/index.js
+++ LAB1_node/backend/db/index.js
@@ -3,6 +3,8 @@ var dotenv = require('dotenv');
 dotenv.config()
 
 const pool = new Pool({
+    //onnectionString: "postgres://ivan:tYuaocd9TrE8K4M02FxXCkNkzF5ZVMSV@dpg-ckqn2aprfc9c739sft4g-a/web2_lab1_2gge",
+    //connectionString: "mysql://ujbtz42hnjir26oo:vAZXe6OGNwhW2MQcUe6h@blamxtxdr4jbeelputqc-mysql.services.clever-cloud.com:3306/blamxtxdr4jbeelputqc"
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
