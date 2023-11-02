698910dadc8883404f79034534606028f9d550ad
unknown <ivanzgelagit@gmail.com>
2023-11-02 22:50:40 +0100

    Added production code

--- LAB2/backend/db/index.js
+++ LAB2/backend/db/index.js
@@ -3,12 +3,12 @@ var dotenv = require('dotenv');
 dotenv.config()
 
 const pool = new Pool({
-    //connectionString: process.env.DB_CONNECTIONSTRING
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DB_CONNECTIONSTRING
+    //user: process.env.DB_USER,
+    //host: process.env.DB_HOST,
+    //database: process.env.DB_DATABASE,
+    //password: process.env.DB_PASS,
+    //port: process.env.DB_PORT,
 });
 
 const sql_create_accounts = `CREATE TABLE accounts (
