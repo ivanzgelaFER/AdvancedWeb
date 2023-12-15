31943797542098e6e3bd9700ce4363e686d2a68f
ivanzgelaFER <ivanzgelagit@gmail.com>
2023-12-15 14:42:23 +0100

    Added offline and 404 page

--- LAB5/public/sw.js
+++ LAB5/public/sw.js
@@ -1,9 +1,10 @@
+//popis zahtjeva koji ce se inicijalno spremiti u cache
 const filesToCache = [
-    "/",
     "manifest.json",
     "index.html",
     "offline.html",
     "404.html",
+    "/",
     "https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
     "https://fonts.gstatic.com/s/firasans/v11/va9E4kDNxMZdWfMOD5Vvl4jLazX3dA.woff2",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
@@ -11,8 +12,9 @@ const filesToCache = [
 
 const staticCacheName = "static-cache-v2";
 
-//install
+//install + cache
 self.addEventListener('install', event => {
+    console.log("Installing lab5 service worker");
     event.waitUntil(
         caches.open(staticCacheName).then((cache) => {
             return cache.addAll(filesToCache);
@@ -21,16 +23,58 @@ self.addEventListener('install', event => {
 });
 
 //activate
-self.addEventListener('activate', event => {
-
+self.addEventListener("activate", (event) => {
+    console.log("Activating lab5 service worker");
+    const cacheWhitelist = [staticCacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
-//fetch
-self.addEventListener('fetch', event => {
- 
+//fetch  
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then((response) => {
+                if (response) {
+                    // console.log("Found " + event.request.url + " in cache!");
+                    //return response;
+                }
+                // console.log("----------------->> Network request for ",
+                //     event.request.url
+                // );
+                return fetch(event.request).then((response) => {
+                    // console.log("response.status = " + response.status);
+                    if (response.status === 404) {
+                        return caches.match("404.html");
+                    }
+                    return caches.open(staticCacheName).then((cache) => {
+                        // console.log(">>> Caching: " + event.request.url);
+                        cache.put(event.request.url, response.clone());
+                        return response;
+                    });
+                });
+            })
+            .catch((error) => {
+                console.log("Error", event.request.url, error);
+                // ovdje možemo pregledati header od zahtjeva i možda vratiti različite fallback sadržaje
+                // za različite zahtjeve - npr. ako je zahtjev za slikom možemo vratiti fallback sliku iz cachea
+                // ali zasad, za sve vraćamo samo offline.html:
+                return caches.match("offline.html");
+            })
+    );
 });
 
-//sync
+//background sync
 self.addEventListener("sync", function (event) {
     console.log("Background sync!", event);
     if (event.tag === "sync-snaps") {
