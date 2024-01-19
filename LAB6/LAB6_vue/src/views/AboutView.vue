81c2bce9d4382c8dbb9db38a2eb05104684e81ac
ivanzgelaFER <ivanzgelagit@gmail.com>
2024-01-19 15:39:01 +0100

    Added about warning#

--- LAB6/LAB6_vue/src/views/AboutView.vue
+++ LAB6/LAB6_vue/src/views/AboutView.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page you need to make project until wednesday</h1>
   </div>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
