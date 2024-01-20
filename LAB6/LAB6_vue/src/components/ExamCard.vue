7a9734b11edeff3f41b91ebd33776636253ed9bc
ivanzgelaFER <ivanzgelagit@gmail.com>
2024-01-20 12:14:53 +0100

    Added new exams

---
+++ LAB6/LAB6_vue/src/components/ExamCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>exam card</div>
+</template>
+
+<script>
+export default {
+  props: [
+    "ispit",
+    "predmet",
+    "dvorana",
+    "ostvareniBodovi",
+    "ukupnoBodova",
+  ],
+  computed: {
+    idUrl() {
+      return "/recipes" + this.id;
+    },
+  },
+  mounted() {
+    console.log("mounted, zoom = " + this.zoom);
+  }
+};
+</script>
+<style scoped>
+div.card-body .badge {
+  white-space: pre-wrap;
+}
+</style>
--- LAB6/LAB6_vue/src/main.js
+++ LAB6/LAB6_vue/src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ExamCard from "./components/ExamCard.vue";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component("exam-card", ExamCard);
 
 app.mount('#app')
