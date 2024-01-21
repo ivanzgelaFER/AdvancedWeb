3d46f0bf091fc35aeb375bf4f042af6c33e48d8b
ivanzgelaFER <ivanzgelagit@gmail.com>
2024-01-21 20:58:36 +0100

    Not found

--- LAB6/LAB6_vue/src/router/index.js
+++ LAB6/LAB6_vue/src/router/index.js
@@ -30,15 +30,11 @@ const router = createRouter({
       name: "login",
       component: () => import('../views/Login.vue'),
     },
-    {
-      path: "/:pathMatch(.*)*",
-      redirect: "/404"
+    { 
+      path: '/:pathMatch(.*)',
+      name: "notFound",
+      component: () => import('../views/NotFound.vue'),
     },
-    {
-        path: "/404",
-        name: "404",
-        component: () => import("../views/NotFound.vue")
-    }
   ]
 });
 
